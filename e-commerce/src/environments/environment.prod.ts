607d5893576da691409a476e5101e4f49ee4e3b4
Parag Jain <iesparagjain@gmail.com>
2025-01-29 14:39:41 +0530

    envs

--- e-commerce/src/environments/environment.prod.ts
+++ e-commerce/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     apiBaseUrl: 'https://e-commerce-backend-nine-tan.vercel.app/api/v1',
-    // apiBaseUrl:'https://6c32-106-214-31-72.ngrok-free.app/api/v1',
     stripePublishableKey:'pk_test_51QkPqnSAOZ17nUFXP7NY9335jNIEaoHpxxn3tt7eAwUzBc5vwAIerYQQQ4VXjviogaPMlGYLuIGy521J5u4O0b3000IXx2plVb'
 };
